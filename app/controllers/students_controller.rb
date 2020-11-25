b6b7986d1c03906ed3aafa93688b2aef62772df8
Michael Iodice <miodice3@gmail.com>
2020-11-25 15:05:10 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show   #this is still showing '/students/:id' it just ommits a lot of text
+    @student = Student.find_by(id: params[:id])
+  end
+
 end
